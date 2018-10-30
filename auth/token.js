2b00cfb42dabca2c195d5b741f772ddab88f05a5
William VanDolah <william.vandolah@gmail.com>
2018-10-30 10:16:58 -0500

    fixed issue with token never expiring

--- auth/token.js
+++ auth/token.js
@@ -5,16 +5,13 @@ const secret = process.env.JWT_SECRET;
  module.exports = { 
     letsMakeAToken(user){
         console.log('inside token',user)
-        const timestamp = new Date().getTime();
         const payload = {
             sub: user.id,
-            iat: timestamp,
             username: user.username,
             externalID: user.externalID,
-            // admin: user.admin
         };
         const options = {
-            expiresIn: '10d'
+            expiresIn: '1h'
         };
         return jwt.sign(payload, secret, options)
     }
